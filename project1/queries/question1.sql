c7c87659270441fdb02357b029e37860cadbd7b7
Nuno <up201807530@fe.up.pt>
2024-03-26 15:08:27 +0000

    Fix query1

--- project1/queries/question1.sql
+++ project1/queries/question1.sql
@@ -1,23 +1,23 @@
 -- x scenario: 
 
 SELECT xucs.codigo, xucs.designacao, xocorrencias.ano_letivo, xocorrencias.inscritos, xtiposaula.tipo, xtiposaula.turnos
-FROM xucs
-JOIN xocorrencias ON xocorrencias.codigo = xucs.codigo
-JOIN xtiposaula ON xtiposaula.codigo = xucs.codigo
+FROM xtiposaula
+LEFT JOIN xocorrencias ON xtiposaula.codigo = xocorrencias.codigo AND xtiposaula.ano_letivo = xocorrencias.ano_letivo AND xtiposaula.periodo = xocorrencias.periodo
+LEFT JOIN xucs ON xocorrencias.codigo = xucs.codigo
 WHERE xucs.curso = 275 AND xucs.designacao = 'Bases de Dados'
 
 -- y scenario:
 
 SELECT yucs.codigo, yucs.designacao, yocorrencias.ano_letivo, yocorrencias.inscritos, ytiposaula.tipo, ytiposaula.turnos
-FROM yucs
-JOIN yocorrencias ON yocorrencias.codigo = yucs.codigo
-JOIN ytiposaula ON ytiposaula.codigo = yucs.codigo
+FROM ytiposaula
+LEFT JOIN yocorrencias ON ytiposaula.codigo = yocorrencias.codigo AND ytiposaula.ano_letivo = yocorrencias.ano_letivo AND ytiposaula.periodo = yocorrencias.periodo
+LEFT JOIN yucs ON yocorrencias.codigo = yucs.codigo
 WHERE yucs.curso = 275 AND yucs.designacao = 'Bases de Dados'
 
 -- z scenario:
 
 SELECT zucs.codigo, zucs.designacao, zocorrencias.ano_letivo, zocorrencias.inscritos, ztiposaula.tipo, ztiposaula.turnos
-FROM zucs
-JOIN zocorrencias ON zocorrencias.codigo = zucs.codigo
-JOIN ztiposaula ON ztiposaula.codigo = zucs.codigo
+FROM ztiposaula
+LEFT JOIN zocorrencias ON ztiposaula.codigo = zocorrencias.codigo AND ztiposaula.ano_letivo = zocorrencias.ano_letivo AND ztiposaula.periodo = zocorrencias.periodo
+LEFT JOIN zucs ON zocorrencias.codigo = zucs.codigo
 WHERE zucs.curso = 275 AND zucs.designacao = 'Bases de Dados'
