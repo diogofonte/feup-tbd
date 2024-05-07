287ef78c79f7eeb278a8bcece35794cd57db80f6
diogofonte <diogo.fonte2000@gmail.com>
2024-05-07 13:23:29 +0100

    feat: progress in data model

--- project2/queries/normalQueries.sql
+++ project2/queries/normalQueries.sql
@@ -2,37 +2,37 @@
 
 --a
 SELECT department_name , COUNT(employee_id) as Employees FROM
-EMPLOYEES
-JOIN departments on departments.department_id = employees.department_id
+DBO_EMPLOYEES
+JOIN DBO_DEPARTMENTS on DBO_DEPARTMENTS.department_id = DBO_EMPLOYEES.department_id
 GROUP BY department_name;
 
 --b
 SELECT department_name, job_title, COUNT(employee_id) as Employees FROM
-EMPLOYEES
-JOIN departments on departments.department_id = employees.department_id
-JOIN jobs on jobs.job_id = employees.job_id
+DBO_EMPLOYEES
+JOIN DBO_DEPARTMENTS on DBO_DEPARTMENTS.department_id = DBO_EMPLOYEES.department_id
+JOIN DBO_JOBS on DBO_JOBS.job_id = DBO_EMPLOYEES.job_id
 GROUP BY department_name, job_title;
 
 
 --c
 SELECT department_name, MAX(salary) as BestPaid  FROM
-EMPLOYEES
-JOIN departments on departments.department_id = employees.department_id
+DBO_EMPLOYEES
+JOIN DBO_DEPARTMENTS on DBO_DEPARTMENTS.department_id = DBO_EMPLOYEES.department_id
 GROUP BY department_name;
 
 --d
 
 SELECT first_name, last_name, (end_date - start_date) as JobDuration FROM 
-employees
-JOIN job_history on job_history.employee_id = employees.employee_id
+DBO_EMPLOYEES
+JOIN DBO_JOB_HISTORY on DBO_JOB_HISTORY.employee_id = DBO_EMPLOYEES.employee_id
 ORDER BY JobDuration DESC;
 
 --e
 SELECT country_name, ROUND(AVG(salary),2) as AverageSalary FROM 
-employees
-JOIN departments on departments.department_id = employees.department_id
-JOIN locations on locations.location_id = departments.location_id
-JOIN countries on countries.country_id = locations.country_id
+DBO_EMPLOYEES
+JOIN DBO_DEPARTMENTS on DBO_DEPARTMENTS.department_id = DBO_EMPLOYEES.department_id
+JOIN DBO_LOCATIONS on DBO_LOCATIONS.location_id = DBO_DEPARTMENTS.location_id
+JOIN DBO_COUNTRIES on DBO_COUNTRIES.country_id = DBO_LOCATIONS.country_id
 GROUP BY country_name;
 
 
